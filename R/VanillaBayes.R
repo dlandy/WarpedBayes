9b2471a163595b470853f29aaa00f879a1c3011f
David Landy <dhlandy@gmail.com>
2017-12-30 11:09:47 -0500

    Added Corrections to pad calls

--- R/VanillaBayes.R
+++ R/VanillaBayes.R
@@ -20,6 +20,9 @@ vanillaBayes <- function (stimuli, ...) {
 
 #' @export
 vanillaBayes.list <- function(stimuli, kappa=0, tauStimuli=1, tauCategory=1, responses="none"){
+  if(length(kappa) == length(stimuli)-2       && length(kappa)>1      ){kappa       <- c(Inf, kappa, Inf)}
+  if(length(tauStimuli) == length(stimuli)-2  && length(tauStimuli)>1 ){tauStimuli  <- c(Inf, tauStimuli, Inf)}
+  if(length(tauCategory) == length(stimuli)-2 && length(tauCategory)>1){tauCategory <- c(Inf, tauCategory, Inf)}
   mapply(vanillaBayes, stimuli, kappa, tauStimuli, tauCategory, responses, SIMPLIFY=FALSE)
 }
 
