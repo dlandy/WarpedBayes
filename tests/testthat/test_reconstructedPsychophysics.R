79c7b3baa67d17ef323a08a88d4eb78ad08da0fc
David Landy <dhlandy@gmail.com>
2018-01-03 12:32:30 -0500

    new tests

--- tests/testthat/test_reconstructedPsychophysics.R
+++ tests/testthat/test_reconstructedPsychophysics.R
@@ -3,9 +3,10 @@ context("bayesianReconstructionFunctions")
 test_that("bayesianGonzalezWu gives warnings and return values", {
   stims <- -10:10
   
-  expect_warning(bayesianGonzalezWu(-5:1, kappa=0.8,tauStimuli=400,tauCategory=100, responses=c(), mode= "simulation")
-                 , "LeftBoundary (-1e-10) larger than smallest stimulus (-2.5)")
-  
+  expect_warning(bayesianGonzalezWu(-5:1/2, kappa=0.8,tauStimuli=400,tauCategory=100, responses=c(), mode= "simulation")
+                 , "LeftBoundary .* larger than smallest stimulus")
+  expect_warning(bayesianGonzalezWu(1:5/2, kappa=0.8,tauStimuli=400,tauCategory=100, responses=c(), mode= "simulation")
+                 , "RightBoundary .* smaller than largest stimulus")
   
 })
 
